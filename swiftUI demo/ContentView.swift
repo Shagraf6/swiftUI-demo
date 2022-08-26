d8754ee34730391333df48f211bef581eda34aaf
Shagraf6 <shagrafnasheet@gmail.com>
2022-08-26 22:09:20 +0500

    Initial Commit

---
+++ swiftUI demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI demo
+//
+//  Created by Aamir Shaikh on 8/26/22.
+//  Copyright © 2022 Gexton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
